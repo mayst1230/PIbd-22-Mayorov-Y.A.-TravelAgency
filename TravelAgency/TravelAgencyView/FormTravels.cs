1ea93adaef3368af50e2d4e0c142ac9922db105f
Mayorov Yury <mayst1230@gmail.com>
2021-02-25 07:56:49 +0400

    fix

--- TravelAgency/TravelAgencyView/FormTravels.cs
+++ TravelAgency/TravelAgencyView/FormTravels.cs
@@ -24,7 +24,7 @@ namespace TravelAgencyView
             InitializeComponent();
             this.logic = logic;
         }
-        private void FormSets_Load(object sender, EventArgs e)
+        private void FormTravels_Load(object sender, EventArgs e)
         {
             LoadData();
         }
