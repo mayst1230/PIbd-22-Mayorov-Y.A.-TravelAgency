efec968c789cf35e708ac31b3b1dec4239609278
mayst1230 <mayst1230@gmail.com>
2021-05-05 11:58:29 +0400

    some fix

--- TravelAgency/TravelAgencyDatabaseImplement/Migrations/20210505060414_InitialCreate.Designer.cs
+++ TravelAgency/TravelAgencyDatabaseImplement/Migrations/20210505060414_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using TravelAgencyDatabaseImplement;
 namespace TravelAgencyDatabaseImplement.Migrations
 {
     [DbContext(typeof(TravelAgencyDatabase))]
-    [Migration("20210501052928_InitialCreate")]
+    [Migration("20210505060414_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
